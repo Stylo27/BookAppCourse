3cb40210626b94892dc639089d572d603e67ffcc
Anton Dymov <anton.dymov@imaginelearning.com>
2023-11-16 18:34:59 +0100

    Wrapped to grids MUI

--- book-store-react/src/App.tsx
+++ book-store-react/src/App.tsx
@@ -2,15 +2,25 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home.component';
 import Book from './components/Book.component';
+import Container from '@mui/material/Container';
+import { Grid } from '@mui/material';
 
 export default function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/books/:bookId" element={<Book/>} />
-      </Routes>
+      <Grid container>
+        <Grid xs={12}>
+          <Navigation />
+        </Grid>
+        <Grid xs={12}>
+          <Container maxWidth="lg">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/books/:bookId" element={<Book/>} />
+            </Routes>
+          </Container>
+        </Grid>
+      </Grid>
     </>
   );
 }
@@ -18,7 +28,11 @@ export default function App() {
 export function Navigation() {
   return(
     <nav>
-      <Link to="/">Home</Link>
+      <Grid container>
+        <Grid xs={12} display={'grid'} justifyContent={'center'}>
+          <Link to="/">Home</Link>
+        </Grid>
+      </Grid>
     </nav>
   )
 }
--- book-store-react/src/components/Home.component.tsx
+++ book-store-react/src/components/Home.component.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { IBook } from '../models/book';
 import { bookService } from '../services/book.service';
 import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
 
 export default function Home() {
   const [books, setBooks] = useState<IBook[]>([]);
@@ -17,21 +18,47 @@ export default function Home() {
   };
 
   return(
-    <table>
-      <tr>
-        <th>Id</th>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Rating</th>
-      </tr>
-      {books.map((book: IBook) => (
-        <tr key={book.id}>
-          <td>{book.id}</td>
-          <td onClick={() => redirectToBook(book.id)}>{book.name}</td>
-          <td>{book.author}</td>
-          <td>{book.rating}</td>
-        </tr>
-      ))}
-    </table>
+    <>
+      <Grid container columnSpacing={2}>
+        <Grid xs={12}>
+          <Grid container>
+            <Grid xs={2}>
+              <span>Id</span>
+            </Grid>
+            <Grid xs={4}>
+              <span>Title</span>
+            </Grid>
+            <Grid xs={4}>
+              <span>Author</span>
+            </Grid>
+            <Grid xs={2}>
+              <span>Rating</span>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid xs={12}>
+          <Grid container>
+            {books.map((book: IBook) => (
+              <Grid xs={12}>
+                <Grid container>
+                    <Grid xs={2}>
+                      <span>{book.id}</span>
+                    </Grid>
+                    <Grid xs={4}>
+                      <span onClick={() => redirectToBook(book.id)}>{book.name}</span>
+                    </Grid>
+                    <Grid xs={4}>
+                      <span>{book.author}</span>
+                    </Grid>
+                    <Grid xs={2}>
+                      <span>{book.rating}</span>
+                    </Grid>
+                </Grid>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   )
 };
